3d6844bd30d390d17a277f2d392eafbedc073505
BAG02 <brian_gulledge@homedepot.com>
2020-01-31 16:06:08 -0500

    Initial commit

---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated //None or Native also
+})
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck {
+
+  @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
+
+  constructor() { 
+    console.log('constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called');
+  }
+
+}
